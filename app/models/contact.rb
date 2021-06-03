a69dee92f547790b8d02dd0b12287a83523539ab
thierrryScotto <thierry_192@outlook.com>
2021-06-03 18:45:41 -0300

    added function kind_description in contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,17 @@ class Contact < ApplicationRecord
   def get_date
     Time.now.to_s
   end
+
+  def kind_descrption
+    self.kind.description
+  end
+
+  def as_json (options={})
+    super(
+      root: true, 
+      exclude: [:kind_id],
+      methods: [:kind_descrption]
+      # include: { kind: { only: :description } } Dessa forma a hash é aninhada dentro da estrutura
+    )
+  end
 end
