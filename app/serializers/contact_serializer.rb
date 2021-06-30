4e722b730d24c7a7497f91e8473a7ebea0d0c40d
thierrryScotto <thierry_192@outlook.com>
2021-06-29 23:30:46 -0300

    added url in of model in json

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,9 +2,13 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   #assossiations
-  belongs_to :kind, optional: true
+  belongs_to :kind, optional: true do
+    link(:kind) { kind_url(object.kind.id) }
+  end
   has_many :phones
   has_one :address
+
+  link(:self) { contact_url(object.id) }
 
   meta do
     { 
