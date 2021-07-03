9d30b32351a7f052e03c669d4733c2664d451fca
thierrryScotto <thierry_192@outlook.com>
2021-07-03 18:32:39 -0300

    address patch, get, post and delete

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,14 +1,42 @@
 class AddressesController < ApplicationController
-  before_action :set_address, only: [:show]
+  before_action :set_address
 
-  # GET /address/1
+  # GET contacts/contacts_id/address
   def show
-    render json: @address
+    render json: @contact.address
+  end
+
+  # POST contacts/contacts_id/address
+  def create
+    @contact.address = Address.new(address_params)
+
+    if @contact.save
+      render @contact.address, status: :created, location: contact_address_url(@contact)
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end 
+  end
+
+  # PATCH contacts/contacts_id/address
+  def update
+    if @contact.address.update(address_params)
+      render json: @contact.address
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @contact.address.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_address
-      @address = Contact.find(params[:contact_id]).address
+      @contact = Contact.find(params[:contact_id])
+    end
+
+    def address_params
+      ActiveModelSerializers::Deserialization.jsonapi_parse(params)
     end
 end
