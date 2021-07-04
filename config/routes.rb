ebc36d26aeeac8acc7987c999bbf9b5675336972
thierrryScotto <thierry_192@outlook.com>
2021-07-03 22:36:57 -0300

    added new routes phones

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
+    resource :phones, only: [:show, :create]
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phones, only: [:show, :create], path: 'relationships/phones'
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
 
     resource :address, only: [:show, :update, :create, :destroy]
     resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
