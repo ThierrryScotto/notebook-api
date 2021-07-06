7436a90ccd7d141a66fe6193ffa314e2df38d396
thierrryScotto <thierry_192@outlook.com>
2021-07-05 22:56:34 -0300

    added new authentication with just http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+  # using the basic format
+  # include ActionController::HttpAuthentication::Basic::ControllerMethods
+  # http_basic_authenticate_with name: "thi", password: "pass"
+  
+  # using the Digest format
+  include ActionController::HttpAuthentication::Digest::ControllerMethods
+  USERS = { "thi" => Digest::MD5.hexdigest(["thi", "Application", "pass"].join(":"))}
+
+  before_action :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_digest("Application") do |username|
+    USERS[username]
+    end
+  end
 end
