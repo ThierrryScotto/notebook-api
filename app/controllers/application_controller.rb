a7f5c060d171d58c8acfb1d7cbd8c00439f45657
thierrryScotto <thierry_192@outlook.com>
2021-07-05 23:12:05 -0300

    added authenticate with token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+
+  TOKEN = "a5c1ca754f514579c5fe31ae6c5d264f"
+
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+
+  private
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      ActiveSupport::SecurityUtils.secure_compare(
+        ::Digest::SHA256.hexdigest(token),
+        ::Digest::SHA256.hexdigest(TOKEN)
+      )
+    end
+  end
 end
