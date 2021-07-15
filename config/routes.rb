1729e9c2691c08ff010873b338ea2555a41b0ea4
thierrryScotto <thierry_192@outlook.com>
2021-07-14 23:33:32 -0300

    added versionamento

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
+  
+  scope module: 'v1' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == '1' }
+  end
+
+  scope module: 'v2' do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == '2' }
+  end
+
   resources :kinds
-  resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
